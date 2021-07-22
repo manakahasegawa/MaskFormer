5a9c70e9bdfe9d83a50c0b778bf7ee1e54af22bb
manakahasegawa <49968217+manakahasegawa@users.noreply.github.com>
2021-07-22 13:11:08 +0900

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
                     assert len(args.input) == 1, "Please specify a directory with args.output"
                     out_filename = args.output
                 visualized_output.save(out_filename)
-                save_image(predictions['sem_seg'], os.path.join(args.output, os.path.basename(path)+'1')))
+                save_image(predictions['sem_seg'], os.path.join(args.output, os.path.basename(path)+'1'))
             else:
                 cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
                 cv2.imshow(WINDOW_NAME, visualized_output.get_image()[:, :, ::-1])
